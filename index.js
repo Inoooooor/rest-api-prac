d74d9ad2cca002483aeb9138f347002e7556b71c
Inoooooor <111541493+Inoooooor@users.noreply.github.com>
2023-09-16 13:20:37 +0300

    added_UserController_methods

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ const PORT = process.env.PORT || 8080
 
 const app = express()
 
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
 app.use("/api", userRouter)
 
 app.listen(PORT, () => console.log(`Server Started on port: ${PORT}`))
