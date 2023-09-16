3c867a7d958bb471498d16406d101864944d701d
Inoooooor <111541493+Inoooooor@users.noreply.github.com>
2023-09-16 12:25:14 +0300

    added_UserController_class&router

---
+++ controllers/user.controller.js
@@ -0,0 +1,13 @@
+class UserConttroller {
+  async createUser(req, res) {}
+
+  async getUsers(req, res) {}
+
+  async getOneUser(req, res) {}
+
+  async updateUser(req, res) {}
+
+  async deleteUser(req, res) {}
+}
+
+module.exports = new UserConttroller()
--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const express = require("express")
 
+const userRouter = require("./routes/user.routes")
+
 const PORT = process.env.PORT || 8080
 
 const app = express()
 
-app.get("/", (req, res) => {
-  res.send("Hello Node JS + PostgreSQL!!!")
-})
+app.use("/api", userRouter)
 
 app.listen(PORT, () => console.log(`Server Started on port: ${PORT}`))
